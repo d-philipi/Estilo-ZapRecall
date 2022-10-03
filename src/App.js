4bf7c47ce2d3807defe3f827cb2c71ce14b5a785
Daniel Philipi <daniphilipi379@gmail.com>
2022-10-03 01:21:05 -0300

    Atualização de imagens

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import styled from 'styled-components';
 export default function App () {
     return(
         <Container>
-            
+            <Logo>
+                <img src='./assets/img/logo.png' alt="Logo do site"/>
+                <h1>ZapRecall</h1>
+            </Logo>
         </Container>
     )
 }
@@ -19,4 +22,22 @@ const Container = styled.div`
     margin: 0px;
     padding: 0px;
     padding-bottom: 200px;
+`
+
+const Logo = styled.div`
+    display: flex;
+    align-items: center;
+    margin: 40px 0 20px 0;
+    img {
+        width: 52px;
+    }
+    h1 {
+        font-family: 'Righteous';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 36px;
+        line-height: 45px;
+        color: #FFFFFF;
+        margin-left: 20px;
+    }
 `
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom';
-import App from './App';
 
-import '../assets/css/reset.css';
-import '../assets/css/style.css';
+import "./assets/css/reset.css";
+
+import App from './App';
 
 ReactDOM.render(<App />, document.querySelector('.root'));
