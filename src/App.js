dcedbbf61b1f73287eeedde3adbb8faee8f77625
Daniel Philipi <daniphilipi379@gmail.com>
2022-10-03 00:49:31 -0300

    Atualização de css

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function App () {
+    return(
+        <Container>
+            
+        </Container>
+    )
+}
+
+const Container = styled.div`
+    background-color: #FB6B6B;
+    width: 100vw;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 0px;
+    padding: 0px;
+    padding-bottom: 200px;
+`
